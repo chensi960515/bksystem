51d5224bf3b1f1c0a28840eebeff1ef6561c4a65
chen <cs960515@outlook.com>
2019-08-27 23:37:17 +0800

    提交前端页面

---
+++ src/main/java/com/cs/bksystem/BkSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cs.bksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BkSystemApplication.class, args);
+  }
+
+}
